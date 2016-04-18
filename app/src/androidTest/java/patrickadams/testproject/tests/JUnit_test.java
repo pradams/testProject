28188016d323b4d89453484c6b24e80202e64964
Patrick <pradams@ucsd.edu>
2016-04-18 11:52:47 -0700

    First commit

---
+++ app/src/androidTest/java/patrickadams/testproject/tests/JUnit_test.java
@@ -0,0 +1,28 @@
+package patrickadams.testproject.tests;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.TextView;
+
+import patrickadams.testproject.MyActivity;
+import patrickadams.testproject.R;
+
+/**
+ * Created by Patrick on 4/18/16.
+ */
+public class JUnit_test extends ActivityInstrumentationTestCase2<MyActivity>{
+
+    MyActivity myActivity;
+    public JUnit_test(){
+        super(MyActivity.class);
+    }
+
+    public void test_first(){
+        myActivity = getActivity();
+
+        TextView textView = (TextView) myActivity.findViewById(R.id.tv1);
+        String tester = textView.getText().toString();
+
+        assertEquals("Bye World", tester);
+    }
+
+}
